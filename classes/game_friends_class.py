82bc8b4a4b384385d664c8ccbcdb520264f9b5bd
Görkem Toppeker <gtoppeker@gmail.com>
2017-11-03 01:31:40 +0300

    GameAnnounce class is defined.

---
+++ classes/game_friends_class.py
@@ -0,0 +1,9 @@
+
+class GameAnnounce:
+    def __init__(self, gameName, gameType, playerNum, gameDate, gameLoc, gameDesc):
+        self.gameName = gameName
+        self.gameType = gameType
+        self.playerNum = playerNum
+        self.gameDate = gameDate
+        self.gameLoc = gameLoc
+        self.gameDesc = gameDesc
