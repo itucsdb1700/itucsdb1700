7a7651c456da88cd8e34b4423ba755395fe197fe
Hakan Sander <hakansander@gmail.com>
2017-11-05 20:38:48 +0300

    Restaurant class is created

---
+++ classes/restaurant_class.py
@@ -0,0 +1,10 @@
+class Restaurant:
+  def __init__(self, locationID, menuType, menuID, restaurantPoint, priceBalance, openingTime, closingTime):
+    self.locationID = locationID
+    self.menuType = menuType
+    self.menuID = menuID
+    self.restaurantPoint = restaurantPoint
+    self.priceBalance = priceBalance
+    self.openingTime = openingTime
+    self.closingTime = closingTime
+
