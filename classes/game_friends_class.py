972d55bb32a65027200440d978b395cb1c4bcd96
Görkem Toppeker <gtoppeker@gmail.com>
2017-11-16 00:59:31 +0300

    GameAnnounce class and sql table edited.

--- classes/game_friends_class.py
+++ classes/game_friends_class.py
@@ -1,9 +1,10 @@
 
 class GameAnnounce:
-    def __init__(self, gameName, gameType, playerNum, gameDate, gameLoc, gameDesc):
+    def __init__(self, gameName, gameType, playerNum, gameDate, gameLoc, gameDesc, user_id):
         self.gameName = gameName
         self.gameType = gameType
         self.playerNum = playerNum
         self.gameDate = gameDate
         self.gameLoc = gameLoc
         self.gameDesc = gameDesc
+        self.user_id = user_id
