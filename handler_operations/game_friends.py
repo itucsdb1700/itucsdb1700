119060d3bb452fe86057aecbd8d3064006b8b87e
Görkem Toppeker <gtoppeker@gmail.com>
2017-11-16 01:00:24 +0300

    Html table is created for Game Friend Page

--- handler_operations/game_friends.py
+++ handler_operations/game_friends.py
@@ -1,6 +1,10 @@
 from flask import render_template
 from flask import current_app
 from flask import request
+from flask_login import current_user
+
+from flask import redirect
+from flask.helpers import url_for
 
 from classes.game_friends_class import GameAnnounce
 
@@ -9,6 +13,12 @@ import psycopg2 as dbapi2
 
 def game_friend_page():
     if request.method == 'POST':
+        # current user information is taken
+        username = current_user.get_username()
+        email = current_user.get_email()
+        # name = current_user.get_name()
+        # surname = current_user.get_surname()
+
         gameName = request.form['InputGameName']
         gameType = request.form['InputGameType']
         if not request.form['GamePlayerNo']:
@@ -19,20 +29,37 @@ def game_friend_page():
         gameLoc = request.form['InputGameLocation']
         gameDesc = request.form['GameDescription']
 
-        gameAnnounce = GameAnnounce(gameName,gameType,playerNum,gameDate,gameLoc,gameDesc)
-
         with dbapi2.connect(current_app.config['dsn']) as connection:
+            # to take the current user's name and email
+            cursor1 = connection.cursor()
+            statement = """SELECT ID FROM USERS WHERE (USERS.USERNAME = %s) AND (USERS.EMAIL = %s)"""
+            cursor1.execute(statement, (username, email))
+            UserId = cursor1.fetchone()
+            UserId = UserId[0]
+
+            gameAnnounce = GameAnnounce(gameName, gameType, playerNum, gameDate, gameLoc, gameDesc, UserId)
+            # print(type(UserId))
+
+            # to insert the new announcement information
             cursor = connection.cursor()
-
-            query = """INSERT INTO GAMEFRIEND (NAME, TYPE, GAMEDATE, LOCATION, PLAYERNUMBER, DESCRIPTION) 
-                                                VALUES('%s', '%s', '%s', '%s', '%d', '%s')""" % (
+            query = """INSERT INTO GAMEFRIEND (NAME, TYPE, GAMEDATE, LOCATION, PLAYERNUMBER, DESCRIPTION, USERID) 
+                                                VALUES('%s', '%s', '%s', '%s', '%d', '%s', '%s')""" % (
                 gameAnnounce.gameName, gameAnnounce.gameType, gameAnnounce.gameDate,
-                gameAnnounce.gameLoc, gameAnnounce.playerNum, gameAnnounce.gameDesc)
-
+                gameAnnounce.gameLoc, gameAnnounce.playerNum, gameAnnounce.gameDesc, gameAnnounce.user_id)
             cursor.execute(query)
             connection.commit()
 
-        return render_template('game_friends.html')
+        #return render_template('game_friends.html')
+        return redirect(url_for('site.GameFriendPage'))
 
     else:
-        return render_template('game_friends.html')
+        with dbapi2.connect(current_app.config['dsn']) as connection:
+            cursor = connection.cursor()
+            query = """SELECT GAMEFRIEND.NAME, TYPE, GAMEDATE, LOCATION, PLAYERNUMBER, DESCRIPTION, 
+            USERS.NAME, USERS.SURNAME, USERS.EMAIL, FACULTIES.FACULTYNAME, FACULTIES.FACULTYCODE 
+            FROM GAMEFRIEND, USERS, FACULTIES 
+            WHERE (GAMEFRIEND.USERID = USERS.ID) AND (USERS.FACULTYID = FACULTIES.ID)"""
+            cursor.execute(query)
+            gameFriendAnnounces = cursor.fetchall()
+
+        return render_template('game_friends.html', gameFriendAnnounces=gameFriendAnnounces)
