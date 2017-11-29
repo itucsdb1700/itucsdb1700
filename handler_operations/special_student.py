a2d5dff2934af525e76421cfd2179fb79129c53a
Sercan <srcnsahan@gmail.com>
2017-11-29 13:11:37 +0300

    added update function to special tutor-student pages

--- handler_operations/special_student.py
+++ handler_operations/special_student.py
@@ -17,32 +17,71 @@ def special_student_page():
         if 'userSearchButton' in request.form:  # if the search button is submitted
             session['search_username'] = request.form['usernameSearch']
             return redirect(url_for('site.SearchUserPage'))
+        formtype = request.form['form-name']
 
         username = current_user.get_username()
         email = current_user.get_email()
         name = current_user.get_name()
         surname = current_user.get_surname()
-        studentsubject = request.form['SpecialStudentSubject']
-        studentname = request.form['SpecialStudentName']
-        if not studentname:
-            seq = {name, surname}
-            studentname = " ".join(seq)
-        studentmail = request.form['SpecialStudentMail']
-        if not studentmail:
-            studentmail = email
-        studentphone = request.form['SpecialStudentPhone']
+        if formtype == "SpecialStudent":
+            studentsubject = request.form['SpecialStudentSubject']
+            studentname = request.form['SpecialStudentName']
+            if not studentname:
+                seq = {name, surname}
+                studentname = " ".join(seq)
+            studentmail = request.form['SpecialStudentMail']
+            if not studentmail:
+                studentmail = email
+            studentphone = request.form['SpecialStudentPhone']
 
-        with dbapi2.connect(current_app.config['dsn']) as connection:
-            cursor = connection.cursor()
-            statement = """SELECT ID FROM USERS WHERE (USERS.USERNAME = %s) AND (USERS.EMAIL = %s)"""
-            cursor.execute(statement, (username, email))
-            studentid = cursor.fetchone()
+            with dbapi2.connect(current_app.config['dsn']) as connection:
+                cursor = connection.cursor()
+                statement = """SELECT ID FROM USERS WHERE (USERS.USERNAME = %s) AND (USERS.EMAIL = %s)"""
+                cursor.execute(statement, (username, email))
+                studentid = cursor.fetchone()
 
-            student = special_student(studentsubject, studentname, studentmail, studentphone, studentid)
-            query = """INSERT INTO SPECIALSTUDENTS(SUBJECT, FULLNAME, EMAIL, PHONENUMBER, USERID) VALUES (%s, %s, %s, %s, %s)"""
-            cursor.execute(query, (student.subject, student.fullname, student.email, student.phonenumber, student.user_id))
-            connection.commit()
-        return redirect(url_for('site.SpecialStudent'))
+                student = special_student(studentsubject, studentname, studentmail, studentphone, studentid)
+                query = """INSERT INTO SPECIALSTUDENTS(SUBJECT, FULLNAME, EMAIL, PHONENUMBER, USERID) VALUES (%s, %s, %s, %s, %s)"""
+                cursor.execute(query, (student.subject, student.fullname, student.email, student.phonenumber, student.user_id))
+                connection.commit()
+            return redirect(url_for('site.SpecialStudent'))
+        elif formtype == "SpecialStudentUpdate":
+            with dbapi2.connect(current_app.config['dsn']) as connection:
+                cursor = connection.cursor()  # prevented sql injection
+                statement = """SELECT ID FROM USERS WHERE (USERS.USERNAME = %s) AND (USERS.EMAIL = %s)"""
+                cursor.execute(statement, (username, email))
+                studentuser_id = cursor.fetchone()
+                studentid = request.form['student-id']
+
+                studentsubject = request.form['SpecialStudentSubject']
+                if not studentsubject:
+                    statement = """SELECT SUBJECT FROM SPECIALSTUDENTS WHERE SPECIALSTUDENTS.ID = %s"""
+                    cursor.execute(statement, studentid)
+                    studentsubject = cursor.fetchone()
+
+                studentname = request.form['SpecialStudentName']
+                if not studentname:
+                    statement = """SELECT FULLNAME FROM SPECIALSTUDENTS WHERE SPECIALSTUDENTS.ID = %s"""
+                    cursor.execute(statement, studentid)
+                    studentname = cursor.fetchone()
+
+                studentmail = request.form['SpecialStudentMail']
+                if not studentmail:
+                    statement = """SELECT EMAIL FROM SPECIALSTUDENTS WHERE SPECIALSTUDENTS.ID = %s"""
+                    cursor.execute(statement, studentid)
+                    studentmail = cursor.fetchone()
+
+                studentphone = request.form['SpecialStudentPhone']
+                if not studentphone:
+                    statement = """SELECT PHONENUMBER FROM SPECIALSTUDENTS WHERE SPECIALSTUDENTS.ID = %s"""
+                    cursor.execute(statement, studentid)
+                    studentphone = cursor.fetchone()
+
+                statement = """UPDATE SPECIALSTUDENTS SET SUBJECT = %s, FULLNAME = %s, EMAIL = %s, PHONENUMBER = %s, USERID = %s WHERE SPECIALSTUDENTS.ID = %s"""
+                cursor.execute(statement, (studentsubject, studentname, studentmail, studentphone, studentuser_id, studentid))
+                connection.commit()
+                return redirect(url_for('site.selected_special_student', studentId=studentid))
+
     else:
         with dbapi2.connect(current_app.config['dsn']) as connection:
             cursor = connection.cursor()
--- handler_operations/special_tutor.py
+++ handler_operations/special_tutor.py
@@ -17,32 +17,71 @@ def special_tutor_page():
         if 'userSearchButton' in request.form: #if the search button is submitted
             session['search_username'] = request.form['usernameSearch']
             return redirect(url_for('site.SearchUserPage'))
+        formtype = request.form['form-name']
 
         username = current_user.get_username()
         email = current_user.get_email()
         name = current_user.get_name()
         surname = current_user.get_surname()
-        tutorsubject = request.form['SpecialTutorSubject']
-        tutorname = request.form['SpecialTutorName']
-        if not tutorname:
-            seq = {name, surname}
-            tutorname = " ".join(seq)
-        tutormail = request.form['SpecialTutorMail']
-        if not tutormail:
-            tutormail = email
-        tutorphone = request.form['SpecialTutorPhone']
+        if formtype == "SpecialTutor":
+            tutorsubject = request.form['SpecialTutorSubject']
+            tutorname = request.form['SpecialTutorName']
+            if not tutorname:
+                seq = {name, surname}
+                tutorname = " ".join(seq)
+            tutormail = request.form['SpecialTutorMail']
+            if not tutormail:
+                tutormail = email
+            tutorphone = request.form['SpecialTutorPhone']
 
-        with dbapi2.connect(current_app.config['dsn']) as connection:
-            cursor = connection.cursor()
-            statement = """SELECT ID FROM USERS WHERE (USERS.USERNAME = %s) AND (USERS.EMAIL = %s)"""
-            cursor.execute(statement, (username, email))
-            tutorid = cursor.fetchone()
+            with dbapi2.connect(current_app.config['dsn']) as connection:
+                cursor = connection.cursor()
+                statement = """SELECT ID FROM USERS WHERE (USERS.USERNAME = %s) AND (USERS.EMAIL = %s)"""
+                cursor.execute(statement, (username, email))
+                tutorid = cursor.fetchone()
 
-            tutor = special_tutor(tutorsubject, tutorname, tutormail, tutorphone, tutorid)
-            query = """INSERT INTO SPECIALTUTORS(SUBJECT, FULLNAME, EMAIL, PHONENUMBER, USERID) VALUES (%s, %s, %s, %s, %s)"""
-            cursor.execute(query, (tutor.subject, tutor.fullname, tutor.email, tutor.phonenumber, tutor.user_id))
-            connection.commit()
-        return redirect(url_for('site.SpecialTutor'))
+                tutor = special_tutor(tutorsubject, tutorname, tutormail, tutorphone, tutorid)
+                query = """INSERT INTO SPECIALTUTORS(SUBJECT, FULLNAME, EMAIL, PHONENUMBER, USERID) VALUES (%s, %s, %s, %s, %s)"""
+                cursor.execute(query, (tutor.subject, tutor.fullname, tutor.email, tutor.phonenumber, tutor.user_id))
+                connection.commit()
+            return redirect(url_for('site.SpecialTutor'))
+        elif formtype == "SpecialTutorUpdate":
+            with dbapi2.connect(current_app.config['dsn']) as connection:
+                cursor = connection.cursor()  # prevented sql injection
+                statement = """SELECT ID FROM USERS WHERE (USERS.USERNAME = %s) AND (USERS.EMAIL = %s)"""
+                cursor.execute(statement, (username, email))
+                tutoruser_id = cursor.fetchone()
+                tutorid = request.form['tutor-id']
+
+                tutorsubject = request.form['SpecialTutorSubject']
+                if not tutorsubject:
+                    statement = """SELECT SUBJECT FROM SPECIALTUTORS WHERE SPECIALTUTORS.ID = %s"""
+                    cursor.execute(statement, tutorid)
+                    tutorsubject = cursor.fetchone()
+
+                tutorname = request.form['SpecialTutorName']
+                if not tutorname:
+                    statement = """SELECT FULLNAME FROM SPECIALTUTORS WHERE SPECIALTUTORS.ID = %s"""
+                    cursor.execute(statement, tutorid)
+                    tutorname = cursor.fetchone()
+
+                tutormail = request.form['SpecialTutorMail']
+                if not tutormail:
+                    statement = """SELECT EMAIL FROM SPECIALTUTORS WHERE SPECIALTUTORS.ID = %s"""
+                    cursor.execute(statement, tutorid)
+                    tutormail = cursor.fetchone()
+
+                tutorphone = request.form['SpecialTutorPhone']
+                if not tutorphone:
+                    statement = """SELECT PHONENUMBER FROM SPECIALTUTORS WHERE SPECIALTUTORS.ID = %s"""
+                    cursor.execute(statement, tutorid)
+                    tutorphone = cursor.fetchone()
+
+                statement = """UPDATE SPECIALTUTORS SET SUBJECT = %s, FULLNAME = %s, EMAIL = %s, PHONENUMBER = %s, USERID = %s WHERE SPECIALTUTORS.ID = %s"""
+                cursor.execute(statement,
+                               (tutorsubject, tutorname, tutormail, tutorphone, tutoruser_id, tutorid))
+                connection.commit()
+                return redirect(url_for('site.selected_special_tutor', tutorId=tutorid))
     else:
         with dbapi2.connect(current_app.config['dsn']) as connection:
             cursor = connection.cursor()
