68978e935cdcd1611f08f58fceb3e94d0e70d89d
Hakan Sander <hakansander@gmail.com>
2017-12-04 00:43:52 +0300

    Restaurant addition is completed

--- classes/restaurant_class.py
+++ classes/restaurant_class.py
@@ -1,10 +1,31 @@
 class Restaurant:
-  def __init__(self, locationID, menuType, menuID, restaurantPoint, priceBalance, openingTime, closingTime):
+  def __init__(self, restaurantName, locationID, menuType, restaurantPoint, openingTime, closingTime, ownerEmail, ownerPhone, serviceType):
+    self.restaurantName = restaurantName
     self.locationID = locationID
     self.menuType = menuType
-    self.menuID = menuID
     self.restaurantPoint = restaurantPoint
-    self.priceBalance = priceBalance
     self.openingTime = openingTime
     self.closingTime = closingTime
+    self.ownerEmail = ownerEmail
+    self.ownerPhone = ownerPhone
+    self.serviceType = serviceType
+
+    def get_restaurant_name(self):
+        return self.restaurantName
+    def get_location_id(self):
+        return self.locationID
+    def get_menu_type(self):
+        return self.menuType
+    def get_restaurant_point(self):
+        return self.restaurantPoint
+    def get_opening_time(self):
+        return self.openingTime
+    def get_closing_time(self):
+        return self.closingTime
+    def get_owner_email(self):
+        return self.ownerEmail
+    def get_owner_phone(self):
+        return self.ownerPhone
+    def get_service_type(self):
+        return self.service_type
 
--- handler_operations/restaurants.py
+++ handler_operations/restaurants.py
@@ -1,35 +1,116 @@
-from flask import render_template, Blueprint
-from flask import current_app
-from datetime import datetime
-from flask import session
-
-from flask import redirect
+from flask import render_template, Blueprint, current_app, session, redirect, request
 from flask.helpers import url_for
-
-from flask_login import login_required
-from flask_login import current_user, login_user, logout_user
-
-from handler_operations.search_user import search_user_page
-from user import User
-from user import get_user
-from flask_login import LoginManager
-from flask import request
-
-from passlib.apps import custom_app_context as pwd_context
+from flask_login import current_user, login_user, logout_user, login_required, LoginManager
 
 import psycopg2 as dbapi2
+
+from classes.restaurant_class import Restaurant
+from server import load_user
+from user import User, get_user
+from passlib.apps import custom_app_context as pwd_context
+from datetime import datetime
 import os.path
 
-from server import load_user
+from classes.lost_stuff_class import lost_stuff
 
-#under development...
 def restaurants_page():
-  if request.method == "POST":
+    if request.method == "POST":
+        if 'userSearchButton' in request.form:  # if the search button is submitted
+            session['search_username'] = request.form['usernameSearch']
+            return redirect(url_for('site.SearchUserPage'))
+        formtype = request.form['form-name']
 
-    if 'userSearchButton' in request.form: #if the search button is submitted
-      session['search_username'] = request.form['usernameSearch']
-      return redirect(url_for('site.SearchUserPage'))
+        username = current_user.get_username()
+        #print(username) #use print to check whether the correct data is retrieved by checking the terminal
+        email = current_user.get_email()
+        #print(email)
+        name = current_user.get_name()
+        #print(name)
+        surname = current_user.get_surname()
+        #print(surname)
+
+        if formtype == "AddRestaurant":
+            restaurantName = request.form['RestaurantName']
+            menuType = request.form['MenuType']
+            campusLocation = request.form['campusLocation']
+            campusLocation = int(campusLocation)
+            restaurantPoint = request.form['RestaurantPoint']
+            restaurantPoint = int(restaurantPoint)
+            openingTime = request.form['OpeningTime']
+            closingTime = request.form['ClosingTime']
+            restaurantOwnerEmail = request.form['RestaurantOwnerEmail']
+            restaurantOwnerPhone = request.form['RestaurantOwnerPhone']
+            serviceType = request.form['RestaurantServiceType']
+
+            with dbapi2.connect(current_app.config['dsn']) as connection:
+                cursor = connection.cursor()  # prevented sql injection
+                restaurant = Restaurant(restaurantName ,campusLocation, menuType, restaurantPoint, openingTime, closingTime, restaurantOwnerEmail, restaurantOwnerPhone, serviceType)
+                query = """INSERT INTO RESTAURANTS(RESTAURANTNAME, LOCATIONID, MENUTYPE, RESTAURANTPOINT, OPENINGTIME, CLOSINGTIME, OWNEREMAIL, OWNERPHONENUMBER, SERVICETYPE) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+                cursor.execute(query, (restaurant.restaurantName, restaurant.locationID, restaurant.menuType, restaurant.restaurantPoint, restaurant.openingTime, restaurant.closingTime, restaurant.ownerEmail, restaurant.ownerPhone, restaurant.serviceType))
+                connection.commit()
+            return redirect(url_for('site.RestaurantsPage'))
+        elif formtype == "RestaurantUpdate":
+            with dbapi2.connect(current_app.config['dsn']) as connection:
+                cursor = connection.cursor()#prevented sql injection
+                statement = """SELECT ID FROM USERS WHERE (USERS.USERNAME = %s) AND (USERS.EMAIL = %s)"""
+                cursor.execute(statement, (username, email))
+                lostuser_id = cursor.fetchone()
+                lostid = request.form['lost-id']
+
+                lostdesc = request.form['LostSomethingDescription']
+                if not lostdesc:
+                    statement = """SELECT STUFFDESC FROM LOSTSTUFF WHERE LOSTSTUFF.ID = %s"""
+                    cursor.execute(statement, lostid)
+                    lostdesc = cursor.fetchone()
+
+                lostlocation = request.form['LostSomethingPossibleLocation']
+                if not lostlocation:
+                    statement = """SELECT POSSIBLELOC FROM LOSTSTUFF WHERE LOSTSTUFF.ID = %s"""
+                    cursor.execute(statement, lostid)
+                    lostlocation = cursor.fetchone()
+
+                lostdate = request.form['LostSomethingDate']
+                if not lostdate:
+                    statement = """SELECT POSSIBLEDATE FROM LOSTSTUFF WHERE LOSTSTUFF.ID = %s"""
+                    cursor.execute(statement,lostid)
+                    lostdate = cursor.fetchone()
+
+                lostname = request.form['LostSomethingOwnerName']
+                if not lostname:
+                    statement = """SELECT OWNERNAME FROM LOSTSTUFF WHERE LOSTSTUFF.ID = %s"""
+                    cursor.execute(statement, lostid)
+                    lostname = cursor.fetchone()
+
+                lostmail = request.form['LostSomethingOwnerMail']
+                if not lostmail:
+                    statement = """SELECT OWNERMAIL FROM LOSTSTUFF WHERE LOSTSTUFF.ID = %s"""
+                    cursor.execute(statement, lostid)
+                    lostmail = cursor.fetchone()
+
+                lostphone = request.form['LostSomethingOwnerPhone']
+                if not lostphone:
+                    statement = """SELECT OWNERPHONE FROM LOSTSTUFF WHERE LOSTSTUFF.ID = %s"""
+                    cursor.execute(statement, lostid)
+                    lostphone = cursor.fetchone()
+
+                statement = """UPDATE LOSTSTUFF SET STUFFDESC=%s, POSSIBLELOC=%s, POSSIBLEDATE=%s, OWNERNAME=%s, OWNERMAIL=%s, OWNERPHONE=%s, USERID=%s WHERE LOSTSTUFF.ID=%s"""
+                cursor.execute(statement, (lostdesc, lostlocation, lostdate, lostname, lostmail, lostphone, lostuser_id, lostid))
+                connection.commit()
+                return redirect(url_for('site.selected_lost_stuff', lostId=lostid))
     else:
-      return render_template('restaurants.html')
-  else:
-    return render_template('restaurants.html')
+        with dbapi2.connect(current_app.config['dsn']) as connection:
+            cursor = connection.cursor()
+            query = """
+                    SELECT RESTAURANTS.LOCATIONID, 
+                      RESTAURANTS.MENUTYPE,
+                      RESTAURANTS.RESTAURANTPOINT,
+                      RESTAURANTS.OPENINGTIME,
+                      RESTAURANTS.CLOSINGTIME,
+                      RESTAURANTS.OWNEREMAIL,
+                      RESTAURANTS.OWNERPHONENUMBER,
+                      RESTAURANTS.SERVICETYPE 
+                      FROM RESTAURANTS
+            """
+            cursor.execute(query)
+            restaurants = cursor.fetchall()
+        return render_template('restaurants.html', restaurants=restaurants)
